6b2c2a5b5815b7b24fb60ffb1793a558918a08a6
Brett Johnson <brett@sdbrett.com>
2019-03-21 16:05:14 +1100

    moved login and out to new file

---
+++ vrlcmSdk/auth.go
@@ -0,0 +1,105 @@
+package vrlcmSdk
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
+
+type LoginResponse struct {
+	Token string `json:"token"`
+}
+
+func CreateLoginRequestBody(u, p string) []byte {
+	bodyString := `{"username":"` + u + `", "password":"` + p + `"}`
+	return []byte(bodyString)
+}
+
+func getLoginResponse(r *http.Response, s *SdkConnection) error {
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return err
+	}
+
+	loginResponse := LoginResponse{}
+
+	err = json.Unmarshal(body, &loginResponse)
+
+	if err != nil {
+		return err
+	}
+
+	s.Token = loginResponse.Token
+
+	return nil
+
+}
+
+func (s *SdkConnection) Login(u, p string) error {
+
+	t := func(c *SdkConnection) {
+		c.Client.Transport = NewDefaultSdkTransport(s.IgnoreCertError)
+	}
+
+	c, _ := NewApiClient(t)
+
+	url := s.BaseUrl + "/login"
+
+	body := CreateLoginRequestBody(u, p)
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(body))
+	if err != nil {
+		return err
+	}
+
+	response, err := c.Client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	err = ValidateHttpResponse(*response)
+	if err != nil {
+		return err
+	}
+	err = getLoginResponse(response, s)
+	if err != nil {
+		return err
+	}
+
+	s.newSdkHeaders()
+
+	return nil
+}
+
+
+func (s *SdkConnection) Logout() error {
+
+	t := func(c *SdkConnection) {
+		c.Client.Transport = NewDefaultSdkTransport(s.IgnoreCertError)
+	}
+
+	c, _ := NewApiClient(t)
+
+	url := s.BaseUrl + "/logout"
+
+
+	req, err := http.NewRequest("POST", url, nil )
+	if err != nil {
+		return err
+	}
+	req.Header = s.headers
+
+	response, err := c.Client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	err = ValidateHttpResponse(*response)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
--- vrlcmSdk/client.go
+++ vrlcmSdk/client.go
@@ -1,9 +1,6 @@
 package vrlcmSdk
 
 import (
-	"bytes"
-	"github.com/hashicorp/packer/common/json"
-	"io/ioutil"
 	"net/http"
 )
 
@@ -16,73 +13,9 @@ type SdkConnection struct {
 }
 
 
-type LoginResponse struct {
-	Token string `json:"token"`
-}
-
-func (s *SdkConnection) Login(u, p string) error {
-
-	t := func(c *SdkConnection) {
-		c.Client.Transport = NewDefaultSdkTransport(s.IgnoreCertError)
-	}
-
-	c, _ := NewApiClient(t)
-
-	url := s.BaseUrl + "/login"
-
-	body := CreateLoginRequestBody(u, p)
-
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(body))
-	if err != nil {
-		return err
-	}
-
-	response, err := c.Client.Do(req)
-	if err != nil {
-		return err
-	}
-
-	err = ValidateHttpResponse(*response)
-	if err != nil {
-		return err
-	}
-	err = getLoginResponse(response, s)
-	if err != nil {
-		return err
-	}
-
-	s.newSdkHeaders()
-
-	return nil
-}
 
 
 
-func getLoginResponse(r *http.Response, s *SdkConnection) error {
-
-	body, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		return err
-	}
-
-	loginResponse := LoginResponse{}
-
-	err = json.Unmarshal(body, &loginResponse)
-
-	if err != nil {
-		return err
-	}
-
-	s.Token = loginResponse.Token
-
-	return nil
-
-}
-
-func CreateLoginRequestBody(u, p string) []byte {
-	bodyString := `{"username":"` + u + `", "password":"` + p + `"}`
-	return []byte(bodyString)
-}
 
 /*
 // TODO Add result parsing and return statement
@@ -116,35 +49,6 @@ func (s *SdkConnection) GetDataCenters() error {
 */
 
 
-func (s *SdkConnection) Logout() error {
-
-	t := func(c *SdkConnection) {
-		c.Client.Transport = NewDefaultSdkTransport(s.IgnoreCertError)
-	}
-
-	c, _ := NewApiClient(t)
-
-	url := s.BaseUrl + "/logout"
-
-
-	req, err := http.NewRequest("POST", url, nil )
-	if err != nil {
-		return err
-	}
-	req.Header = s.headers
-
-	response, err := c.Client.Do(req)
-	if err != nil {
-		return err
-	}
-
-	err = ValidateHttpResponse(*response)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
 
 
 
