d6317e4237a960fd77824d7ea9513a922e7cfeac
Brett Johnson <brett@sdbrett.com>
2019-03-22 08:39:12 +1100

    Updated README

---
+++ README.md
@@ -0,0 +1,5 @@
+#### vRealize Lifecycle Manager
+
+This project provide an SDK for vRealize Suite Lifecycle Manager written in Go.
+
+The project is in the initial development stage and not fit for real use.
---
+++ vrlcmSdk/doc.go
@@ -0,0 +1,11 @@
+// Copyright 2019 Brett Johnson (sdbrett). All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+//
+// Overview
+//	Package vrlcmSdk provides an SDK for VMware vRealize Suite
+//	lifecycle manager.
+
+
+
+package main
