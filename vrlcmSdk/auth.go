c07c8952af6a72d939229642d92ca832b2b3e418
Brett Johnson <brett@sdbrett.com>
2019-03-22 08:51:44 +1100

    Added comments for goDoc

--- vrlcmSdk/auth.go
+++ vrlcmSdk/auth.go
@@ -19,36 +19,40 @@ func CreateLoginRequestBody(u, p string) []byte {
 	return []byte(bodyString)
 }
 
-//
+// Retrieves auth token from login API call response
 func getAuthToken(r *http.Response) (string, error) {
 
+	// Parse response body
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		return "", err
 	}
 
+	// Marshall response body into loginResponse struct
 	loginResponse := LoginResponse{}
-
 	err = json.Unmarshal(body, &loginResponse)
-
 	if err != nil {
 		return "", err
 	}
 
+	// Return token code
 	return loginResponse.Token, nil
 
 }
 
+// Performs authentication function with vRLCM server
+// Adds auth token string to the SdkConnection
 func (s *SdkConnection) Login(u, p string) error {
 
+	// Setup http transport using default transport
 	t := func(c *SdkConnection) {
 		c.Client.Transport = NewDefaultSdkTransport(s.IgnoreCertError)
 	}
 
+	// Create new http client
 	c, _ := NewApiClient(t)
 
 	url := s.BaseUrl + "/login"
-
 	body := CreateLoginRequestBody(u, p)
 
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(body))
@@ -70,17 +74,21 @@ func (s *SdkConnection) Login(u, p string) error {
 		return err
 	}
 
+	// Create new headers for SDK connection which will contain the auth token
 	s.newSdkHeaders()
 
 	return nil
 }
 
+// Performs logout action against vRLCM server
 func (s *SdkConnection) Logout() error {
 
+	// Setup http transport using default transport
 	t := func(c *SdkConnection) {
 		c.Client.Transport = NewDefaultSdkTransport(s.IgnoreCertError)
 	}
 
+	// Create new http client
 	c, _ := NewApiClient(t)
 
 	url := s.BaseUrl + "/logout"
--- vrlcmSdk/doc.go
+++ vrlcmSdk/doc.go
@@ -1,11 +1,12 @@
 // Copyright 2019 Brett Johnson (sdbrett). All rights reserved.
 // Use of this source code is governed by a MIT style
 // license that can be found in the LICENSE file.
+
+// SDK for vRealize Suite Lifecycle Manager
+// Introduction
 //
-// Overview
-//	Package vrlcmSdk provides an SDK for VMware vRealize Suite
-//	lifecycle manager.
-
-
-
-package main
+// This package provides an Go SDK for vRealize Suite Lifecycle Manager
+// to simplify interaction with the products API.
+//
+// The project is not supplied or supported by VMware and support is community based.
+package vrlcmSdk
