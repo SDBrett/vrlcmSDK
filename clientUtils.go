9b92c59cf6de546caa33d5efd51595c669ebfa97
Brett Johnson <brett@sdbrett.com>
2019-05-10 07:41:48 +1000

    removed unsed functions

--- clientUtils.go
+++ clientUtils.go
@@ -1,20 +1,8 @@
 package vrlcmsdk
 
 import (
-	"errors"
-	"io/ioutil"
 	"net/http"
 )
-
-func ValidateHttpResponse(r http.Response) error {
-
-	if r.StatusCode >= 300 {
-		b, _ := ioutil.ReadAll(r.Body)
-		err := errors.New(string(b))
-		return err
-	}
-	return nil
-}
 
 func (cli *ApiClient) setDefaultHeaders() {
 
--- clientUtils_test.go
+++ clientUtils_test.go
@@ -1,54 +1,8 @@
 package vrlcmsdk
 
 import (
-	"bytes"
-	"fmt"
-	"io/ioutil"
-	"math/rand"
-	"net/http"
 	"testing"
-	"time"
 )
-
-func newHttpResponseCodes(count int) []int {
-
-	rand.Seed(time.Now().UTC().UnixNano())
-	var s []int
-	min := 200
-	max := 500
-
-	for i := 0; i <= count; i++ {
-		s = append(s, rand.Intn(max-min)+min)
-		fmt.Println()
-	}
-
-	return s
-}
-
-func TestValidateHttpResponse(t *testing.T) {
-
-	responseCodes := newHttpResponseCodes(20)
-	r := ioutil.NopCloser(bytes.NewReader([]byte("hello world")))
-	resp := &http.Response{Body: r}
-	for _, c := range responseCodes {
-
-		resp.StatusCode = c
-
-		err := ValidateHttpResponse(*resp)
-
-		if c <= 299 {
-			if err != nil {
-				t.Errorf("expected no error for statuscode : %d", resp.StatusCode)
-			}
-		}
-		if c >= 300 {
-			if err == nil {
-				t.Errorf("expected error for status code : %d", resp.StatusCode)
-			}
-		}
-	}
-
-}
 
 func TestApiClient_setDefaultHeaders(t *testing.T) {
 
