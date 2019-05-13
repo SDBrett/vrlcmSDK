a0f60e0c82b07c32ffc248e26c4410b58e1ed93d
Brett Johnson <brett@sdbrett.com>
2019-05-14 08:40:57 +1000

    added test for new client with http client

--- client_test.go
+++ client_test.go
@@ -1,20 +1,40 @@
 package vrlcmsdk
 
-import "testing"
+import (
+	"net/http"
+	"testing"
+)
 
 func TestNewApiClient(t *testing.T) {
 
-	ignoreSSL := true
-	host := "https://192.168.17.128"
+	t.Run("Test create new client without http client", func(t *testing.T) {
 
-	c := NewApiClient(host, ignoreSSL, nil)
+		ignoreSSL := true
+		host := "https://192.168.17.128"
 
-	if c.AllowInsecure != ignoreSSL {
-		t.Errorf("SSL verify setting does not match input, expected %t, received %t", c.AllowInsecure, ignoreSSL)
-	}
+		c := NewApiClient(host, ignoreSSL, nil)
 
-	if c.Host != host {
-		t.Errorf("Host setting does not match input, expected %s, received %s", c.Host, host)
-	}
+		if c.AllowInsecure != ignoreSSL {
+			t.Errorf("SSL verify setting does not match input, expected %t, received %t", c.AllowInsecure, ignoreSSL)
+		}
+
+		if c.Host != host {
+			t.Errorf("Host setting does not match input, expected %s, received %s", c.Host, host)
+		}
+	})
+
+	t.Run("Test create new client with http client", func(t *testing.T) {
+
+		ignoreSSL := true
+		host := "https://192.168.17.128"
+
+		client := &http.Client{}
+
+		c := NewApiClient(host, ignoreSSL, client)
+
+		if c.Host != host {
+			t.Errorf("Host setting does not match input, expected %s, received %s", c.Host, host)
+		}
+	})
 
 }
