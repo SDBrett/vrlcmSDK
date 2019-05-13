6b5312e0e5bb6bed98415f4503af822e14c25e20
Brett Johnson <brett@sdbrett.com>
2019-05-14 09:16:14 +1000

    updated tests for new structure

--- auth_test.go
+++ auth_test.go
@@ -1,148 +1,153 @@
 package vrlcmsdk
 
 import (
+	"bytes"
 	"context"
-	"log"
+	"fmt"
+	"io/ioutil"
 	"net/http"
-	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
-func TestCreateLoginRequestBody(t *testing.T) {
-
-}
 func TestApiClient_Login(t *testing.T) {
 
 	var ctx = context.Background()
-	authToken := "TOKENCODE"
+	expectedURL := "127.0.0.1/lcm/api/v1/login"
 
-	responseBody := `{"Token": "` + authToken + `"}`
-	// Mock http server
+	t.Run("Test successful vCenter login", func(t *testing.T) {
 
-	ts := httptest.NewServer(
-		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			if r.URL.Path == "/lcm/api/v1/login" {
-				w.Header().Add("Content-Type", "application/json")
-				_, err := w.Write([]byte(responseBody))
-				if err != nil {
-					log.Fatalf("Error writing response")
-				}
+		serverResponse := `{"token":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ4biIsInN1YiI6Ii9jb3JlL2F1dGh6L3VzZXJzL3ZMQ01BZG1pbiIsImV4cCI6NDcxMTc4ODMxN30.LBFSBgdwr9T2xh0qD0ElcfqpWozP_C6SNmXvWG9vz44"}`
+		expectedToken := "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ4biIsInN1YiI6Ii9jb3JlL2F1dGh6L3VzZXJzL3ZMQ01BZG1pbiIsImV4cCI6NDcxMTc4ODMxN30.LBFSBgdwr9T2xh0qD0ElcfqpWozP_C6SNmXvWG9vz44"
+
+		cli := newMockClient(func(req *http.Request) (*http.Response, error) {
+			if !strings.HasPrefix(req.URL.Path, expectedURL) {
+				return nil, fmt.Errorf("expected URL '%s', got '%s'", expectedURL, req.URL)
 			}
-			if r.URL.Path == "/fail/lcm/api/v1/login" {
-				w.Header().Add("Content-Type", "application/json")
-				http.Error(w, "Error", http.StatusUnauthorized)
-			}
-			if r.URL.Path == "/404/lcm/api/v1/login" {
-				http.Error(w, "Error", http.StatusNotFound)
+			if req.Method != "POST" {
+				return nil, fmt.Errorf("expected GET method, got %s", req.Method)
 			}
 
-		}),
-	)
-	defer ts.Close()
+			return &http.Response{
+				StatusCode: http.StatusOK,
+				Body:       ioutil.NopCloser(bytes.NewReader([]byte(serverResponse))),
+			}, nil
+		})
 
-	t.Run("Test successful login", func(t *testing.T) {
-
-		ignoreSSL := true
-		c := NewApiClient(ts.URL, ignoreSSL, nil)
+		c := NewApiClient("127.0.0.1", true, cli)
 
 		err := c.Login(ctx, "admin@localhost", "vmware")
 		if err != nil {
-			t.Errorf("no error response")
+			t.Errorf("expected no error during login")
 		}
-		if c.token != authToken {
-			t.Errorf("expected token code %s, received %s", authToken, c.token)
+		if c.token != expectedToken {
+			t.Errorf("expected token code did not match result")
 		}
 	})
 
-	t.Run("Test successful login", func(t *testing.T) {
+	t.Run("Test bad JSON decoding", func(t *testing.T) {
 
-		ignoreSSL := true
-		uri := ts.URL + "/fail"
+		serverResponse := `{"message":"Unparseable JSON body: com.google.gson.stream.MalformedJsonException: Expected name at line 4 column 2 path $.","statusCode":400,"documentKind":"com:vmware:xenon:common:ServiceErrorResponse","errorCode":0}`
+		cli := newMockClient(func(req *http.Request) (*http.Response, error) {
+			if !strings.HasPrefix(req.URL.Path, expectedURL) {
+				return nil, fmt.Errorf("expected URL '%s', got '%s'", expectedURL, req.URL)
+			}
+			if req.Method != "POST" {
+				return nil, fmt.Errorf("expected GET method, got %s", req.Method)
+			}
 
-		c := NewApiClient(uri, ignoreSSL, nil)
+			return &http.Response{
+				StatusCode: http.StatusBadRequest,
+				Body:       ioutil.NopCloser(bytes.NewReader([]byte(serverResponse))),
+			}, nil
+		})
+
+		c := NewApiClient("127.0.0.1", true, cli)
 
 		err := c.Login(ctx, "admin@localhost", "vmware")
 		if err == nil {
-			t.Errorf("expected error response, no response received")
+			t.Errorf("expected error to be thrown when with server error response")
 		}
 	})
 
-	t.Run("Test bad url login", func(t *testing.T) {
+	t.Run("Test server response error for vCenter creation", func(t *testing.T) {
 
-		ignoreSSL := true
-		uri := ts.URL + "/404"
-		c := NewApiClient(uri, ignoreSSL, nil)
+		serverResponse := "TEST"
+		cli := newMockClient(func(req *http.Request) (*http.Response, error) {
+			if !strings.HasPrefix(req.URL.Path, expectedURL) {
+				return nil, fmt.Errorf("expected URL '%s', got '%s'", expectedURL, req.URL)
+			}
+			if req.Method != "POST" {
+				return nil, fmt.Errorf("expected GET method, got %s", req.Method)
+			}
 
-		err := c.Login(ctx, "username", "password")
+			return &http.Response{
+				StatusCode: http.StatusOK,
+				Body:       ioutil.NopCloser(bytes.NewReader([]byte(serverResponse))),
+			}, nil
+		})
+
+		c := NewApiClient("127.0.0.1", true, cli)
+
+		err := c.Login(ctx, "admin@localhost", "vmware")
 		if err == nil {
-			t.Errorf("expected error response, no response received")
+			t.Errorf("expected error to be thrown when decoding fails")
 		}
 	})
-
 }
 
 func TestApiClient_Logout(t *testing.T) {
 
 	var ctx = context.Background()
-	// Mock http server
-	ts := httptest.NewServer(
-		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			if r.URL.Path == "/lcm/api/v1/login/logout" {
-				w.Header().Add("Content-Type", "application/json")
+	expectedURL := "127.0.0.1/lcm/api/v1/logout"
+
+	t.Run("Test successful vCenter logout", func(t *testing.T) {
+
+		serverResponse := ""
+		cli := newMockClient(func(req *http.Request) (*http.Response, error) {
+			if !strings.HasPrefix(req.URL.Path, expectedURL) {
+				return nil, fmt.Errorf("expected URL '%s', got '%s'", expectedURL, req.URL)
 			}
-			if r.URL.Path == "/fail/lcm/api/v1/logout" {
-				http.Error(w, "Error", http.StatusUnauthorized)
-			}
-			if r.URL.Path == "/404/lcm/api/v1/logout" {
-				http.Error(w, "Error", http.StatusNotFound)
+			if req.Method != "POST" {
+				return nil, fmt.Errorf("expected GET method, got %s", req.Method)
 			}
 
-		}),
-	)
-	defer ts.Close()
+			return &http.Response{
+				StatusCode: http.StatusOK,
+				Body:       ioutil.NopCloser(bytes.NewReader([]byte(serverResponse))),
+			}, nil
+		})
 
-	t.Run("Test successful logout", func(t *testing.T) {
+		c := NewApiClient("127.0.0.1", true, cli)
 
-		ignoreSSL := true
-		c := NewApiClient(ts.URL, ignoreSSL, nil)
 		err := c.Logout(ctx)
 		if err != nil {
-			t.Errorf("no error response")
-		}
-	})
-	t.Run("Test failed logout", func(t *testing.T) {
-
-		uri := ts.URL + "/fail"
-		ignoreSSL := true
-
-		c := NewApiClient(uri, ignoreSSL, nil)
-		err := c.Logout(ctx)
-		if err == nil {
-			t.Errorf("expected error response, no response received")
+			t.Errorf("expected no error during login")
 		}
 	})
 
-	t.Run("Test bad URI logout", func(t *testing.T) {
+	t.Run("Test server error response", func(t *testing.T) {
 
-		uri := "https://fail"
-		ignoreSSL := true
+		serverResponse := `{"message":"Unparseable JSON body: com.google.gson.stream.MalformedJsonException: Expected name at line 4 column 2 path $.","statusCode":400,"documentKind":"com:vmware:xenon:common:ServiceErrorResponse","errorCode":0}`
+		cli := newMockClient(func(req *http.Request) (*http.Response, error) {
+			if !strings.HasPrefix(req.URL.Path, expectedURL) {
+				return nil, fmt.Errorf("expected URL '%s', got '%s'", expectedURL, req.URL)
+			}
+			if req.Method != "POST" {
+				return nil, fmt.Errorf("expected GET method, got %s", req.Method)
+			}
 
-		c := NewApiClient(uri, ignoreSSL, nil)
+			return &http.Response{
+				StatusCode: http.StatusBadRequest,
+				Body:       ioutil.NopCloser(bytes.NewReader([]byte(serverResponse))),
+			}, nil
+		})
+
+		c := NewApiClient("127.0.0.1", true, cli)
+
 		err := c.Logout(ctx)
 		if err == nil {
-			t.Errorf("expected error response, no response received")
-		}
-	})
-
-	t.Run("Test bad url logout", func(t *testing.T) {
-
-		uri := ts.URL + "/404"
-		ignoreSSL := true
-
-		c := NewApiClient(uri, ignoreSSL, nil)
-		err := c.Logout(ctx)
-		if err == nil {
-			t.Errorf("expected error response, no response received")
+			t.Errorf("expected error to be thrown when with server error response")
 		}
 	})
 }
