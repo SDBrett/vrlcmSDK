b0a0ca78210eec895ed98510b8393499ca76d09c
Brett Johnson <brett@sdbrett.com>
2019-05-14 06:14:12 +1000

    Added vCenter and request types

--- types/requests.go
+++ types/requests.go
@@ -1,1 +1,16 @@
 package types
+
+type Request struct {
+	ID              string                   `json:"id"`
+	Type            string                   `json:"type"`
+	State           string                   `json:"state"`
+	Status          string                   `json:"status"`
+	IsRetriable     bool                     `json:"isRetriable"`
+	RetryParameters []RequestRetryParameters `json:"retryParameters"`
+}
+
+type RequestRetryParameters struct {
+	EventId   string `json:"eventId"`
+	MessageId string `json:"messageId"`
+	Message   string `json:"message"`
+}
--- types/vcenter.go
+++ types/vcenter.go
@@ -1,1 +1,11 @@
 package types
+
+type Vcenter struct {
+	Name                 string `json:"vCenterName"`
+	Username             string `json:"userName"`
+	Password             string `json:"password"`
+	DatacenterName       string `json:"datacenterName"`
+	Type                 int    `json:"type"`
+	Datacenters          string `json:"vCDataCenteres"`
+	TemplateCustomSpecs  string `json:"templateCustomSpecs"`
+}
