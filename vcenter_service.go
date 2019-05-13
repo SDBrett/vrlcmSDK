d6d243fdb418f2490b40b9fe35a1c9258f51fed6
Brett Johnson <brett@sdbrett.com>
2019-05-14 08:46:12 +1000

    vcenter API service

--- vcenter_service.go
+++ vcenter_service.go
@@ -1,1 +1,32 @@
 package vrlcmsdk
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/sdbrett/vrlcmsdk/types"
+)
+
+// Vcenter API service
+type VcenterAPIService service
+
+// Create Vcenter server object, returns request object
+func (service *VcenterAPIService) Create(ctx context.Context, vc *types.Vcenter) (types.Request, error) {
+
+	url := service.client.basePath + "/action/add/vc"
+	var request types.Request
+
+
+	resp, err := service.client.post(ctx, url, vc, *service.client.headers)
+	if err != nil {
+		return request, err
+	}
+
+	err = json.NewDecoder(resp.body).Decode(&request)
+	if err != nil {
+		return request, err
+	}
+
+	ensureReaderClosed(resp)
+
+	return request, nil
+}
--- vcenter_service_test.go
+++ vcenter_service_test.go
@@ -1,1 +1,103 @@
 package vrlcmsdk
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"github.com/sdbrett/vrlcmsdk/types"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"testing"
+)
+
+// Test vCenter server object creation for vCenter API service
+func TestVcenterAPIService_Create(t *testing.T) {
+
+	var ctx = context.Background()
+	expectedURL := "127.0.0.1/lcm/api/v1/action/add/vc"
+
+	t.Run("Test successful vCenter creation", func(t *testing.T) {
+
+		serverResponse := `{"id":"2c551b23da979e75588cb94147e9a","type":"VC_DATA_COLLECTION","state":null,"status":"SUBMITTED","isRetriable":null,"retryParameters":null}`
+
+		cli := newMockClient(func(req *http.Request) (*http.Response, error) {
+			if !strings.HasPrefix(req.URL.Path, expectedURL) {
+				return nil, fmt.Errorf("expected URL '%s', got '%s'", expectedURL, req.URL)
+			}
+			if req.Method != "POST" {
+				return nil, fmt.Errorf("expected GET method, got %s", req.Method)
+			}
+
+			return &http.Response{
+				StatusCode: http.StatusOK,
+				Body:       ioutil.NopCloser(bytes.NewReader([]byte(serverResponse))),
+			}, nil
+		})
+
+		newVcenter := types.Vcenter{Name: "TestDataCenter", Username: "admin@local.com", Password: "pass", DatacenterName: "dc1", Type: 3,}
+		c := NewApiClient("127.0.0.1", true, cli)
+
+		request, err := c.VcenterService.Create(ctx, &newVcenter)
+		if err != nil {
+			t.Errorf("expected no error when creating datacenter")
+		}
+		if request.ID != "2c551b23da979e75588cb94147e9a" {
+			t.Errorf("expected ID 2c551b23da979e75588cb94147e9a, instead got %s", request.ID)
+		}
+	})
+
+
+	t.Run("Test bad JSON decoding", func(t *testing.T) {
+
+		serverResponse := `{"message":"A vCenter with the provided name is already attached to this data center. Use PATCH operation to update the vCenter details.","messageId":"12706","statusCode":400,"documentKind":"com:vmware:xenon:common:ServiceErrorResponse","errorCode":0}`
+		cli := newMockClient(func(req *http.Request) (*http.Response, error) {
+			if !strings.HasPrefix(req.URL.Path, expectedURL) {
+				return nil, fmt.Errorf("expected URL '%s', got '%s'", expectedURL, req.URL)
+			}
+			if req.Method != "POST" {
+				return nil, fmt.Errorf("expected GET method, got %s", req.Method)
+			}
+
+			return &http.Response{
+				StatusCode: http.StatusBadRequest,
+				Body:       ioutil.NopCloser(bytes.NewReader([]byte(serverResponse))),
+			}, nil
+		})
+
+		newVcenter := types.Vcenter{Name: "TestDataCenter", Username: "admin@local.com", Password: "pass", DatacenterName: "dc1", Type: 3,}
+		c := NewApiClient("127.0.0.1", true, cli)
+
+		_, err := c.VcenterService.Create(ctx, &newVcenter)
+		if err == nil {
+			t.Errorf("expected error to be thrown when with server error response")
+		}
+	})
+
+	t.Run("Test server response error for vCenter creation", func(t *testing.T) {
+
+		serverResponse := "TEST"
+		cli := newMockClient(func(req *http.Request) (*http.Response, error) {
+			if !strings.HasPrefix(req.URL.Path, expectedURL) {
+				return nil, fmt.Errorf("expected URL '%s', got '%s'", expectedURL, req.URL)
+			}
+			if req.Method != "POST" {
+				return nil, fmt.Errorf("expected GET method, got %s", req.Method)
+			}
+
+			return &http.Response{
+				StatusCode: http.StatusOK,
+				Body:       ioutil.NopCloser(bytes.NewReader([]byte(serverResponse))),
+			}, nil
+		})
+
+		newVcenter := types.Vcenter{Name: "TestDataCenter", Username: "admin@local.com", Password: "pass", DatacenterName: "dc1", Type: 3,}
+		c := NewApiClient("127.0.0.1", true, cli)
+
+		_, err := c.VcenterService.Create(ctx, &newVcenter)
+		if err == nil {
+			t.Errorf("expected error to be thrown when decoding fails")
+		}
+	})
+
+}
